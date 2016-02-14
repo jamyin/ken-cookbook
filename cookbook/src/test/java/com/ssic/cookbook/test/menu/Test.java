c29f606eddf04ac1587245afe5b8727126d3a5ad
rkzhang <rongkun_zhang@ssic.cn>
2016-02-14 17:47:41 +0800

    test commit

---
+++ cookbook/src/test/java/com/ssic/cookbook/test/menu/Test.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.ssic.cookbook.test.menu;
+
+/**		
+ * <p>Title: Test </p>
+ * <p>Description: 类描述</p>
+ * <p>Copyright (c) 2016 </p>
+ * <p>Company: 上海天坊信息科技有限公司</p>
+ * @author rkzhang	
+ * @date 2016年2月14日 下午5:47:24	
+ * @version 1.0
+ * <p>修改人：rkzhang</p>
+ * <p>修改时间：2016年2月14日 下午5:47:24</p>
+ * <p>修改备注：</p>
+ */
+public class Test {
+
+    /**   
+     * 创建一个新的实例 Test.      
+     */
+    public Test() {
+	// TODO Auto-generated constructor stub
+    }
+
+}
+
